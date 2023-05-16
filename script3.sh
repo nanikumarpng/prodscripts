7d3a3f4273a91647ef708ae77b92b6acee8c35d9
knanikumarpng <knanikumar777@gmail.com>
2023-05-16 10:42:41 +0530

    version 1.0

---
+++ script3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Enter A value ="
+read a
+echo "Enter B value ="
+read b
+c=`expr $a + $b`
+echo "C value =$c"
+##########################
+#!/bin/bash
+echo "Enter A value ="
+read a
+echo "Enter B value ="
+read b
+c=`expr $a - $b`
+echo "C value =$c"
+##########################
+#!/bin/bash
+echo "Enter A value ="
+read a
+echo "Enter B value ="
+read b
+c=`expr $a \* $b`
+echo "C value =$c"
+##########################
+#!/bin/bash
+echo "Enter A value ="
+read a
+echo "Enter B value ="
+read b
+c=`expr $a / $b`
+echo "C value =$c"
+##########################
